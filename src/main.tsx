7f29cc61dd9830f92e7223db2c2ad4e1e1a356d1
Sebas <sebastiantorres.develop@gmail.com>
2026-04-04 14:49:21 +0200

    Provider implementation

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './Front/styles/index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./Front/styles/index.css";
+import App from "./App.tsx";
+import { WarehouseProvider } from "./Front/Provider.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+createRoot(document.getElementById("root")!).render(
+  <WarehouseProvider>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </WarehouseProvider>,
+);
