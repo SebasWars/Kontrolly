b10eed2c5a0f10585ca4b1f0190782dc6f939939
Sebas <sebastiantorres.develop@gmail.com>
2026-04-04 14:46:45 +0200

    add dynamic parameter

--- src/Front/Routes.tsx
+++ src/Front/Routes.tsx
@@ -23,7 +23,7 @@ export const routes = createBrowserRouter([
             element: <Stock />,
           },
           {
-            path: "anadir-nuevo-item",
+            path: ":id/anadir-nuevo-item",
             element: <CreateNewItem/>
           }
         ],
