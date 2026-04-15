8e5d73138ca6710cf0d2da17918960b957989f20
Sebas <sebastiantorres.develop@gmail.com>
2026-04-15 13:55:28 +0200

    Strat backend

---
+++ src/Back/App.js
@@ -0,0 +1,61 @@
+import express from "express";
+import multer from "multer";
+import cors from "cors";
+
+import { Stocks } from "./MockData_Back.js";
+import { randomUUID } from "node:crypto";
+
+const PORT = process.env.PORT ?? 3000;
+const app = express();
+const upload = multer({ dest: "uploads/" });
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
+
+app.get("/", (req, res) => {
+  const warehouses = Stocks;
+  return res.json({ warehouses });
+});
+
+app.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const warehouse = Stocks.find((W) => W.id === id);
+  if (!warehouse) {
+    return res.json({ message: "Warehouse non-existent" });
+  }
+  return res.json({ warehouse: warehouse });
+});
+
+app.post("/:id", upload.single("file"), (req, res) => {
+  const { id } = req.params;
+  const warehouse = Stocks.find((W) => W.id === id);
+
+  if (!warehouse) {
+    return res.json({ message: "Warehouse non-existent" });
+  }
+
+  const file = req.file;
+  const { name, description, quantity, purchase_price, sales_price } = req.body;
+
+  const newItem = {
+    id: randomUUID(),
+    name,
+    description,
+    quantity,
+    purchase_price,
+    sales_price,
+    image: file ? file.filename : null,
+    image_path: file ? file.path : null,
+  };
+
+  warehouse.items.push(newItem);
+  return res.status(201).json({ message: "New item created", item: newItem });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto http://localhost:${PORT}`);
+});
