564cbe295490f138c780a3cd471e1c917f642a0a
Sebas <sebastiantorres.develop@gmail.com>
2026-04-18 13:24:59 +0200

    remove Mockdata front, new case

--- src/Front/Reducer/warehouseReducer.ts
+++ src/Front/Reducer/warehouseReducer.ts
@@ -1,14 +1,17 @@
-import { Stocks } from "../../MOCK/StocksMock";
 import type { Actions, State } from "../Types/ReducerTypes";
 
 export const initalState: State = {
-  warehouses: Stocks,
+  warehouses: [],
   selectedWarehouse: null,
 };
 
 export const warehouseReducer = (state: State, action: Actions) => {
   const { type, payload } = action;
   switch (type) {
+    /* WAREHOUSES FATHER ACTIONS */
+    case "SET_WAREHOUSES":
+      return { ...state, warehouses: payload };
+    /* WAREHOUSE CHILDREN ACTIONS */
     case "SELECT_WAREHOUSE":
       return { ...state, selectedWarehouse: payload };
     default:
