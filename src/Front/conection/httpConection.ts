ad98f696e8de55a80c0e960902c6c23f6e19a79a
Sebas <sebastiantorres.develop@gmail.com>
2026-04-18 13:27:33 +0200

    POST method for warehouses

--- src/Front/conection/httpConection.ts
+++ src/Front/conection/httpConection.ts
@@ -1,9 +1,26 @@
-const apiUrl = import.meta.env.VITE_API_URL
+import type { CreateStockType } from "../Types/StockTypes";
 
-export async function getWarehouses(){
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    return data
+const apiUrl = import.meta.env.VITE_API_URL;
+
+export async function getWarehouses() {
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  return data;
 }
 
-export function getWarehousebyID(){}
+export async function createNewWarehouse(newWarehouse: CreateStockType) {
+  const response = await fetch(`${apiUrl}/inventario`, {
+    method: "POST",
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newWarehouse),
+  });
+  if (!response.ok) {
+    throw new Error("Error creating new warehouse");
+  }
+  const data = await response.json();
+  return data;
+}
+
+export function getWarehousebyID() {}
