c18f9bb0c15ef441399e0ad799aa28b971afa7f0
Sebas <sebastiantorres.develop@gmail.com>
2026-04-21 14:37:50 +0200

    Update Stockactions and utils

--- src/Front/Hooks/useStockActions.tsx
+++ src/Front/Hooks/useStockActions.tsx
@@ -1,17 +1,20 @@
 import { useNavigate } from "react-router-dom";
 import useWarehouse from "./UseWarehouse";
+import { useFetchDataByID } from "./useFetchWarehouses";
 
 export function useStockActions() {
-  const { selectedWarehouse, dispatch } = useWarehouse();
+  const { selectedWarehouseId, dispatch } = useWarehouse();
+  const {fetchWarehousesById} = useFetchDataByID()
   const navigate = useNavigate();
 
   const handleSelector = (value: string) => {
     dispatch({ type: "SELECT_WAREHOUSE", payload: value });
+    fetchWarehousesById(value)
   };
 
   const goToNewItem = () => {
-    if (!selectedWarehouse) return;
-    navigate(`${selectedWarehouse?.id}/anadir-nuevo-item`);
+    if (!selectedWarehouseId) return;
+    navigate(`${selectedWarehouseId}/anadir-nuevo-item`);
   };
 
   return { handleSelector, goToNewItem };
--- src/Front/Utils/StockUtils.ts
+++ src/Front/Utils/StockUtils.ts
@@ -1,13 +1,18 @@
-import type { StocksTypes } from "../Types/StockTypes";
+import type { Items, WarehousesMeta } from "../Types/StockTypes";
 
-export const _calculateQuantity = (obj: StocksTypes | null): number => {
-  return obj?.items.length || 0;
+export const _calculateQuantity = (items: Items[] | null): number => {
+  return items?.length || 0;
 };
 
-export const _calculateInvesment = (obj: StocksTypes | null): number => {
-  return obj?.items.reduce((acc, item) => acc + item.purchase_price, 0) || 0;
+export const _calculateInvesment = (items: Items[] | null): number => {
+  return items?.reduce((acc, item) => acc + item.purchase_price, 0) || 0;
 };
 
-export const _calculateProfits = (obj: StocksTypes | null): number => {
-  return obj?.items.reduce((acc, item) => acc + item.sales_price, 0) || 0;
+export const _calculateProfits = (items: Items[] | null): number => {
+  return items?.reduce((acc, item) => acc + item.sales_price, 0) || 0;
+};
+
+export const getWarehouseName = (arr: WarehousesMeta[], id: string) => {
+  const currentWarehosue = arr.find((Wname: WarehousesMeta) => Wname.id === id);
+  return currentWarehosue?.warehouse;
 };
