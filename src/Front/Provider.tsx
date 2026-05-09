e53ea8b7e512a09a5d4507dea094efbf90e18607
Sebas <sebastiantorres.develop@gmail.com>
2026-05-10 01:53:00 +0200

    Update provider for new feature 'sales'

--- src/Front/Provider.tsx
+++ src/Front/Provider.tsx
@@ -1,15 +1,26 @@
 import { createContext, useReducer } from "react";
 import { initalState, warehouseReducer } from "./Reducer/warehouseReducer";
-import type { PropProviderType, WarehouseContextType } from "./Types/ProviderTypes";
+import type {
+  PropProviderType,
+  WarehouseContextType,
+} from "./Types/ProviderTypes";
 
-export const WarehouseContext = createContext<WarehouseContextType | undefined>(undefined);
+export const WarehouseContext = createContext<WarehouseContextType | undefined>(
+  undefined,
+);
 
 export function WarehouseProvider({ children }: PropProviderType) {
   const [state, dispatch] = useReducer(warehouseReducer, initalState);
 
   return (
     <WarehouseContext.Provider
-      value={{warehouses: state.warehouses, selectedWarehouseId: state.selectedWarehouseId, warehouseItems: state.warehouseItems, dispatch}}
+      value={{
+        warehouses: state.warehouses,
+        selectedWarehouseId: state.selectedWarehouseId,
+        warehouseItems: state.warehouseItems,
+        stocksForSales: state.stocksForSales,
+        dispatch,
+      }}
     >
       {children}
     </WarehouseContext.Provider>
