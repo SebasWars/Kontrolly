2ef8d43881cd9fbfb76719a0b15b235eb5981c1b
Sebas <sebastiantorres.develop@gmail.com>
2026-05-10 01:56:19 +0200

    Sales feature implementation.

--- src/Front/Pages/Sells.tsx
+++ src/Front/Pages/Sells.tsx
@@ -1,5 +1,19 @@
-function Sells(){
-    return <h1>Sells</h1>
+import { SalesItemsGrid } from "../components/sales/SalesItemsGrid";
+import { SalesResume } from "../components/sales/SalesResume";
+import { SalesSearch } from "../components/sales/SalesSearch";
+
+import "../styles/sales.css";
+
+function Sells() {
+  return (
+    <div className="sales_container">
+      <section className="sales_left_container">
+        <SalesSearch />
+        <SalesItemsGrid />
+      </section>
+      <SalesResume/>
+    </div>
+  );
 }
 
-export default Sells
+export default Sells;
