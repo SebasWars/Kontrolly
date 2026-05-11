8fe17ae9770fe6b8fcdc42a500590551816886b4
Sebas <sebastiantorres.develop@gmail.com>
2026-05-12 00:39:14 +0200

    modify sales components

--- src/Front/Pages/Sells.tsx
+++ src/Front/Pages/Sells.tsx
@@ -1,17 +1,40 @@
+import { useEffect } from "react";
 import { SalesItemsGrid } from "../components/sales/SalesItemsGrid";
 import { SalesResume } from "../components/sales/SalesResume";
 import { SalesSearch } from "../components/sales/SalesSearch";
+import { useFetchWarehouses } from "../Hooks/StockHooks/useFetchWarehouses";
+import useWarehouse from "../Hooks/StockHooks/UseWarehouse";
 
 import "../styles/sales.css";
+import { useFetchSalesItems } from "../Hooks/SellsHooks/useFetchSales";
 
 function Sells() {
+  const { dispatch, selectWarehouseSalesId, itemsSales } = useWarehouse();
+  const {getItemsforSale} = useFetchSalesItems()
+  /* MOVER ESTO AL COMPONENTE PADRE EN CUANTO LO NECESTE,  */
+  const { fetchWarehouses } = useFetchWarehouses();
+
+  const handleSelectorSales = (value: string) => {
+    dispatch({ type: "SET_WAREHOUSE_SALES", payload: value });
+    getItemsforSale(value)
+    /* FETCH PARA LA LISTA DE LOS ITEMS */
+  };
+
+  useEffect(() => {
+    fetchWarehouses();
+    console.log(itemsSales);
+  }, [selectWarehouseSalesId]);
+
   return (
     <div className="sales_container">
       <section className="sales_left_container">
-        <SalesSearch />
+        <SalesSearch
+          handleChange={handleSelectorSales}
+          stockForSalesID={selectWarehouseSalesId || ""}
+        />
         <SalesItemsGrid />
       </section>
-      <SalesResume/>
+      <SalesResume />
     </div>
   );
 }
