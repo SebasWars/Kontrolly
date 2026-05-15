e0214aa52b8fc8a4f478f1d2aad759459060302a
Sebas <sebastiantorres.develop@gmail.com>
2026-05-15 14:20:04 +0200

    Establish an HTTP connection to create a sale

--- src/Front/services/salesHTTP.ts
+++ src/Front/services/salesHTTP.ts
@@ -1,3 +1,5 @@
+import type { SaleItems } from "../Types/SalesTypes";
+
 const apiUrl = import.meta.env.VITE_API_URL;
 
 export async function getItems(id: string) {
@@ -6,5 +8,22 @@ export async function getItems(id: string) {
     throw new Error("No items found");
   }
   const data = await response.json();
-  return data
+  return data;
+}
+
+export async function createSell(cart: SaleItems[], warehouseID: string) {
+  const items = cart.map(({ id, quantity }) => ({ id, quantity }));
+
+  const response = await fetch(`${apiUrl}/ventas/${warehouseID}`, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({ items }),
+  });
+
+  if (!response.ok) {
+    throw new Error("It was not posible to complete the sell.");
+  }
+  return await response.json()
 }
