51d2af7cb0b7164a0417144f05e0845d00b1bfb0
Sebas <sebastiantorres.develop@gmail.com>
2026-06-23 10:43:36 +0200

    Complete component and fix styles

--- src/Front/Pages/ModifyInvoice.tsx
+++ src/Front/Pages/ModifyInvoice.tsx
@@ -1,17 +1,31 @@
 import { InvoiceDetails } from "../components/invoicesDetails/InvoiceDetails";
 import { PDFPreview } from "../components/invoicesDetails/InvoicePreview";
 import { InvoiceTableItems } from "../components/invoicesDetails/InvoiceTableItems";
-import '../styles/modifyInvoice.css'
+import "../styles/modifyInvoice.css";
+
+import useInvoices from "../Hooks/UseInvoices";
+import { useInvoice } from "../Hooks/InvoicesHooks/useInvoice";
 
 export function ModifyInvoice() {
+  const { invoiceDetails, addOne, removeOne, removeItem } = useInvoices();
+  useInvoice();
+
   return (
     <div className="modify_invoice_main_container">
       <div className="invoice_details_main_container">
-        <InvoiceDetails />
-        <InvoiceTableItems/>
+        <InvoiceDetails
+          invoiceID={invoiceDetails.id}
+          invoiceDate={invoiceDetails.createdAt}
+        />
+        <InvoiceTableItems
+          invoiceItems={invoiceDetails.itemsList}
+          addOne={addOne}
+          removeOne={removeOne}
+          removeItem={removeItem}
+        />
       </div>
       <div className="invoice_preview_container">
-        <PDFPreview/>
+        <PDFPreview />
       </div>
     </div>
   );
