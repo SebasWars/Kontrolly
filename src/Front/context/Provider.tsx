355f2278bfe6fda2408351788e7932f2b532728b
Sebas <sebastiantorres.develop@gmail.com>
2026-07-01 19:15:26 +0200

    provide provider

--- src/Front/context/Provider.tsx
+++ src/Front/context/Provider.tsx
@@ -3,21 +3,20 @@ import { WarehouseProvider } from "./Providers/WarehouseProvider";
 import { PopupProvider } from "./Providers/PopupProvider";
 import { SalesProvider } from "./Providers/SalesProvider";
 import { InvoicesProvider } from "./Providers/InvoiceProvider";
-
+import { ClientsProvider } from "./Providers/ClientsProvider";
 
 export interface PropProviderType {
   children: ReactNode;
 }
-
 
 export const AppProviders = ({ children }: PropProviderType) => {
   return (
     <PopupProvider>
       <WarehouseProvider>
         <SalesProvider>
-          <InvoicesProvider>
-          {children}
-          </InvoicesProvider>
+          <ClientsProvider>
+            <InvoicesProvider>{children}</InvoicesProvider>
+          </ClientsProvider>
         </SalesProvider>
       </WarehouseProvider>
     </PopupProvider>
