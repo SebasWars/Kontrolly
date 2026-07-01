b617b001143d2a9fdfdd427f81a7eca5136f68f3
Sebas <sebastiantorres.develop@gmail.com>
2026-07-01 19:14:53 +0200

    ger client resume service

--- src/Front/services/clientsHTTP.ts
+++ src/Front/services/clientsHTTP.ts
@@ -2,16 +2,23 @@ import type { Client } from "../Pages/Clients";
 
 const apiUrl = import.meta.env.VITE_API_URL;
 
+export const getClientsResume = async () => {
+  const response = await fetch(`${apiUrl}/clientes/resumen`);
+  const data = await response.json();
+  return data;
+};
+
 export const getClients = async () => {
   const response = await fetch(`${apiUrl}/clientes`);
   const data = await response.json();
   console.log(data);
+  return data;
 };
 
 export const getClientByID = async (cliendId: string) => {
   const response = await fetch(`${apiUrl}/clientes/cliente/${cliendId}`);
   const data = await response.json();
-  console.log(data);
+  return data;
 };
 
 export const createClient = async (clientForm: Client) => {
@@ -23,5 +30,5 @@ export const createClient = async (clientForm: Client) => {
     body: JSON.stringify(clientForm),
   });
   const data = await response.json();
-  console.log(data)
+  return data
 };
