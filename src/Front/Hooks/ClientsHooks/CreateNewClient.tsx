211cf3f64824be62af43bb1ef83efefe68f58924
Sebas <sebastiantorres.develop@gmail.com>
2026-07-02 18:40:23 +0200

    create client hook

---
+++ src/Front/Hooks/ClientsHooks/CreateNewClient.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import type { NewClient } from "../../context/RecuderTypes/ClientsReduce";
+
+export function CreateClient() {
+  const [newClient, setNewClient] = useState<NewClient>({
+    companyName: "",
+    name: "",
+    address: "",
+    emailAddress: "",
+    phoneNumber: "",
+    notes: "",
+  });
+  const formHandler = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    const { name, value } = e.target;
+    setNewClient((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const validateForm = () => {
+    if (!newClient.companyName.trim()) {
+      alert("La empresa es obligatoria");
+      return false;
+    }
+
+    if (!newClient.name.trim()) {
+      alert("El nombre es obligatorio");
+      return false;
+    }
+
+    if (!newClient.address.trim()) {
+      alert("La dirrección es obligatoria");
+      return false;
+    }
+
+    if (!newClient.emailAddress.trim()) {
+      alert("El correo es obligatorio");
+      return false;
+    }
+    if (!newClient.phoneNumber.trim()) {
+      alert("El numero de telefono es obligatorio");
+      return false;
+    }
+
+    return true;
+  };
+
+  return { newClient, formHandler, validateForm };
+}
