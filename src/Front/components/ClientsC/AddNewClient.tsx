a50ff924aa22c50880e9778d1f71ee75e7143711
Sebas <sebastiantorres.develop@gmail.com>
2026-07-02 18:39:46 +0200

    clean add client component

--- src/Front/components/ClientsC/AddNewClient.tsx
+++ src/Front/components/ClientsC/AddNewClient.tsx
@@ -1,52 +1,104 @@
-type PropTypes = {
-    toggleForm : (val: boolean) => void 
-}
+import { useFetchClients } from "../../Hooks/ClientsHooks/useFetchClients";
+import { CreateClient } from "../../Hooks/ClientsHooks/CreateNewClient";
 
-export function AddNewClient({toggleForm}: PropTypes) {
+type PropTypes = {
+  toggleForm: (val: boolean) => void;
+};
+
+export function AddNewClient({ toggleForm }: PropTypes) {
+  const { createNewClient } = useFetchClients();
+  const { newClient, formHandler, validateForm } = CreateClient();
+
+  const createClient = () => {
+    if (!validateForm()) return;
+    createNewClient(newClient);
+    toggleForm(false);
+  };
+
   return (
     <div className="add_new_client_container">
       <header className="add_client_header">
         <h2>Añadir nuevo cliente</h2>
-        <button onClick={() => toggleForm(false)} className="close_client_form_btn">x</button>
+        <button
+          onClick={() => toggleForm(false)}
+          className="close_client_form_btn"
+        >
+          x
+        </button>
       </header>
 
       <div className="add_client_form">
         <form action="">
-          <label className="company_name" htmlFor="">
+          <label className="company_name" htmlFor="companyName">
             Empresa
-            <input type="text" name="" id="" />
+            <input
+              value={newClient.companyName}
+              onChange={formHandler}
+              type="text"
+              name="companyName"
+              id="companyName"
+            />
           </label>
           <div className="form_section">
-            <label htmlFor="">
+            <label htmlFor="name">
               Contacto directo
-              <input type="text" name="" id="" />
+              <input
+                value={newClient.name}
+                onChange={formHandler}
+                type="text"
+                name="name"
+                id="name"
+              />
             </label>
-            <label htmlFor="">
+            <label htmlFor="address">
               Dirección
-              <input type="text" name="" id="" />
+              <input
+                value={newClient.address}
+                onChange={formHandler}
+                type="text"
+                name="address"
+                id="address"
+              />
             </label>
           </div>
           <div className="form_section">
-            <label htmlFor="">
+            <label htmlFor="phoneNumber">
               Numero de contacto
-              <input type="text" name="" id="" />
+              <input
+                value={newClient.phoneNumber}
+                onChange={formHandler}
+                type="text"
+                name="phoneNumber"
+                id="phoneNumber"
+              />
             </label>
-            <label htmlFor="">
+            <label htmlFor="emailAddress">
               Correo electronico
-              <input type="email" name="" id="" />
+              <input
+                value={newClient.emailAddress}
+                onChange={formHandler}
+                type="email"
+                name="emailAddress"
+                id="emailAddress"
+              />
             </label>
           </div>
 
-          <label htmlFor="">
+          <label htmlFor="notes">
             Notas
-            <textarea name="" id=""></textarea>
+            <textarea
+              value={newClient.notes}
+              onChange={formHandler}
+              name="notes"
+              id="notes"
+            ></textarea>
           </label>
         </form>
       </div>
 
       <div className="new_client_action_buttons">
         <button onClick={() => toggleForm(false)}>Cancelar</button>
-        <button>Guardar</button>
+        <button onClick={createClient}>Guardar</button>
       </div>
     </div>
   );
