e54478cbc456681c37f5eb754ff6dbbeeeec5cd7
Sebas <sebastiantorres.develop@gmail.com>
2026-07-08 12:20:10 +0200

    Clean clients component

--- src/Front/Pages/Clients.tsx
+++ src/Front/Pages/Clients.tsx
@@ -1,11 +1,12 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "../styles/clienst.css";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-import { AddNewClient } from "../components/ClientsC/AddNewClient";
 import { useState } from "react";
-import { useCreateClient } from "../Hooks/ClientsHooks/CreateNewClient";
-import type { Client } from "../context/RecuderTypes/ClientsReduce";
+
+import { AddNewClient } from "../components/ClientsC/AddNewClient";
+import { ClientSearch } from "../components/ClientsC/ClientSearch";
 import { TableClients } from "../components/ClientsC/ClientsTable";
+
+import type { Client } from "../context/RecuderTypes/ClientsReduce";
+import { useCreateClient } from "../Hooks/ClientsHooks/CreateNewClient";
 
 export function Clients() {
   const { clientForm, toggleForm } = useCreateClient();
@@ -25,10 +26,7 @@ export function Clients() {
       </header>
       <div className="clientes_dashboard">
         <div className="title">
-          <div className="input">
-            <FontAwesomeIcon icon={faMagnifyingGlass} />
-            <input className="search_client" type="text" />
-          </div>
+          <ClientSearch />
         </div>
         <div className="clients_table">
           <TableClients
