b5c67b2e925ac5a0262782e99108427063dacf29
Sebas <sebastiantorres.develop@gmail.com>
2026-07-09 11:39:25 +0200

    Authorization back and middleware

--- src/Back/App.js
+++ src/Back/App.js
@@ -1,14 +1,18 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import { warehousesRoute } from "./routes/stock.js";
 import { salesRoute } from "./routes/sales.js";
 import { invoicesRoute } from "./routes/invoices.js";
 import { homeRoute } from "./routes/home.js";
 import { clientsRoute } from "./routes/clients.js";
+import { authRoute } from "./routes/autho.js";
+import { verifyJWT } from "./middleware/verifyJWT.js";
 
 export const PORT = process.env.PORT ?? 3000;
 const app = express();
+dotenv.config();
 
 app.use(express.json());
 app.use(
@@ -18,12 +22,13 @@ app.use(
 );
 app.use("/uploads", express.static("uploads"));
 
-app.use('/', homeRoute)
-app.use('/inventario', warehousesRoute)
-app.use('/tienda', salesRoute)
-app.use('/facturas', invoicesRoute)
-app.use('/clientes',clientsRoute)
+app.use("/auth", authRoute);
 
+app.use("/", verifyJWT, homeRoute);
+app.use("/inventario", verifyJWT, warehousesRoute);
+app.use("/tienda", verifyJWT, salesRoute);
+app.use("/facturas", verifyJWT, invoicesRoute);
+app.use("/clientes", verifyJWT, clientsRoute);
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto http://localhost:${PORT}`);
---
+++ src/Back/routes/autho.js
@@ -0,0 +1,32 @@
+import { Router } from "express";
+import { users } from "../MockData_Back.js";
+import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken'
+import { use } from "react";
+
+export const authRoute = Router();
+
+authRoute.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = users.find((user) => user.email === email);
+
+  if (!user) {
+    return res.status(404).json({ message: "Usuario no encontrado" });
+  }
+
+  const validatePassword = await bcrypt.compare(password, user.passwordHash);
+  if (!validatePassword) {
+    return res.status(401).json({ message: "Password invalid" });
+  }
+
+  const token = jwt.sign({
+    id: user.id,
+    email: user.email,
+    isActive: user.isActive
+  },process.env.JWT_SECRET,{
+    expiresIn: '1h'
+  })
+
+  res.json({ message: "Login correcto", token });
+});
+
