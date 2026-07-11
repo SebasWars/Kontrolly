2c7091f51994a7bc164aa3dcbaf5ca2a8e838f0f
Sebas <sebastiantorres.develop@gmail.com>
2026-07-11 14:00:53 +0200

    Refactor: update MVC back

--- src/Back/routes/autho.js
+++ src/Back/routes/autho.js
@@ -1,38 +1,7 @@
 import { Router } from "express";
-import { users } from "../MockData_Back.js";
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import { use } from "react";
+import { authorizationController } from "../Controller/authorization.js";
 
 export const authRoute = Router();
 
-authRoute.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  const user = users.find((user) => user.email === email);
-
-  if (!user) {
-    return res.status(404).json({ message: "Usuario no encontrado" });
-  }
-
-  const validatePassword = await bcrypt.compare(password, user.passwordHash);
-  if (!validatePassword) {
-    return res.status(401).json({ message: "Password invalid" });
-  }
-
-  const token = jwt.sign(
-    {
-      id: user.id,
-      email: user.email,
-      isActive: user.isActive,
-    },
-    process.env.JWT_SECRET,
-    {
-      expiresIn: "1h",
-    },
-  );
-
-  res.json({
-    token,
-    user: { id: user.id, name: user.name, email: user.email },
-  });
-});
+authRoute.post("/login", authorizationController.logIn);
