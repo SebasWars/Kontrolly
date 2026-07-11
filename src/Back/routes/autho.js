4d74419e937843cb4fc3b1e00555a130567076ff
Sebas <sebastiantorres.develop@gmail.com>
2026-07-11 15:59:04 +0200

    Feat: Create user

--- src/Back/routes/autho.js
+++ src/Back/routes/autho.js
@@ -5,3 +5,4 @@ import { authorizationController } from "../Controller/authorization.js";
 export const authRoute = Router();
 
 authRoute.post("/login", authorizationController.logIn);
+authRoute.post('/createUser', authorizationController.createUser)
