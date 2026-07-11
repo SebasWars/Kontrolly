4a61fec60c2ae833fb35c0d31ae6e93744c4c888
Sebas <sebastiantorres.develop@gmail.com>
2026-07-11 16:09:46 +0200

    Refactor: Separeta forms from components

--- src/Front/components/LogIn/CreateAccount.tsx
+++ src/Front/components/LogIn/CreateAccount.tsx
@@ -1,9 +1,6 @@
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useState } from "react";
-import type { NewAccount } from "../../Types/LogInTypes";
-import { createAccount } from "../../services/LoginHTTP";
-import { useAuthorization } from "../../Hooks/UseAuthorization";
+import { useCreateAccount } from "../../Hooks/LogIn/UseCreateAccount";
 
 interface PropTypes {
   showPassword: boolean;
@@ -16,31 +13,7 @@ export function CreateAccount({
   passwordVisibility,
   changeForm,
 }: PropTypes) {
-  const { login } = useAuthorization();
-  const [newAccount, setNewAccount] = useState<NewAccount>({
-    companyName: "",
-    email: "",
-    password: "",
-    passwordConfirm: "",
-  });
-
-  const newAccountHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setNewAccount((prev) => ({ ...prev, [name]: value }));
-  };
-
-  const createUser = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    try {
-      if (newAccount.password !== newAccount.passwordConfirm) {
-        alert("La contraseña no coincide con la confirmación.");
-      }
-      const data = await createAccount(newAccount);
-      login(data.token, data.user);
-    } catch (error) {
-      throw new Error("It was not possible to create a new account.");
-    }
-  };
+  const { newAccount, newAccountHandler, createUser } = useCreateAccount();
 
   return (
     <div className="create_account_form">
