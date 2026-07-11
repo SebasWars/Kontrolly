e5afa1f46f538e24d379fc2602d8c747f726dc43
Sebas <sebastiantorres.develop@gmail.com>
2026-07-11 15:59:53 +0200

    Feat and Refactor: Create user back and refactor error cases

--- src/Back/Controller/authorization.js
+++ src/Back/Controller/authorization.js
@@ -6,15 +6,39 @@ export class authorizationController {
       const { email, password } = req.body;
       const user = await authorizationModel.userAuthorization(email, password);
 
-      if (!user) {
-        return res.status(404).json({ message: "Unexistence user" });
+      if (!user.success && user.error === "INVALID_USER") {
+        return res.status(404).json({ message: "User not found" });
+        return;
+      }
+
+      if (!user.success && user.error === "INVALID_PASSWORD") {
+        return res.status(401).json({ message: "Password incorrect" });
       }
 
       return res.status(200).json(user);
     } catch (error) {
-      return res.status(404).json({
-        message:
-          "It was not possible to log in, please verify the information, once again.",
+      return res.status(500).json({
+        message: "Internal server error",
+      });
+    }
+  }
+
+  static async createUser(req, res) {
+    try {
+      const userForm = req.body;
+      const newUser = await authorizationModel.createUser(userForm);
+
+      if (!newUser.success && newUser.error === "EMAIL_ALREADY_EXISTS") {
+        return res.status(409).json({
+          message:
+            "This email address is already associated with another account.",
+        });
+      }
+
+      return res.status(201).json(newUser);
+    } catch (error) {
+      return res.status(500).json({
+        message: "Internal server error",
       });
     }
   }
