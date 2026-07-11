641f7285ad73a7c9f644678ce466d04cd710d3f5
Sebas <sebastiantorres.develop@gmail.com>
2026-07-11 13:32:32 +0200

    Feat: Create login backend process

--- src/Back/routes/autho.js
+++ src/Back/routes/autho.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { users } from "../MockData_Back.js";
 import bcrypt from "bcrypt";
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 import { use } from "react";
 
 export const authRoute = Router();
@@ -19,14 +19,20 @@ authRoute.post("/login", async (req, res) => {
     return res.status(401).json({ message: "Password invalid" });
   }
 
-  const token = jwt.sign({
-    id: user.id,
-    email: user.email,
-    isActive: user.isActive
-  },process.env.JWT_SECRET,{
-    expiresIn: '1h'
-  })
+  const token = jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      isActive: user.isActive,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "1h",
+    },
+  );
 
-  res.json({ message: "Login correcto", token });
+  res.json({
+    token,
+    user: { id: user.id, name: user.name, email: user.email },
+  });
 });
-
