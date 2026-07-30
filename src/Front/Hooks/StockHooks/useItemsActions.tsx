648bc10f9a98b04f187635ce68954965a61b5842
Sebas <sebastiantorres.develop@gmail.com>
2026-07-30 15:06:28 +0200

    FIX: Update warehouse items when an item was created.

--- src/Front/Hooks/StockHooks/useItemsActions.tsx
+++ src/Front/Hooks/StockHooks/useItemsActions.tsx
@@ -2,9 +2,13 @@ import { useNavigate } from "react-router-dom";
 import { createNewItem } from "../../services/httpConection";
 import type { NewItem } from "../../Types/StockTypes";
 import { validateNewItem } from "../../Utils/validation";
+import { useFetchDataByID } from "./useFetchWarehouses";
+import useWarehouse from "../UseWarehouse";
 
 export function useItemsActions() {
   const navigate = useNavigate();
+  const { fetchWarehousesById } = useFetchDataByID();
+  const { selectedWarehouseId } = useWarehouse();
 
   const createItem = async (id: string, data: NewItem) => {
     const errors = validateNewItem(data);
@@ -14,6 +18,8 @@ export function useItemsActions() {
     }
 
     await createNewItem(id, data);
+    if(!selectedWarehouseId) return;
+    await fetchWarehousesById(selectedWarehouseId);
     navigate("/inventario");
   };
 
